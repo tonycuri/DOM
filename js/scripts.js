bc1fe5f988fc77097b84f61337a17b03bdac4e9c
Tony Curi <curicaballero@gmail.com>
2020-06-07 11:48:45 -0300

    aprendimos a usar getElementById - getElementsByClassName, getElementsByTagName para seleccionar contenido en Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,36 @@
   'use strict';
   document.addEventListener('DOMContentLoaded', function(){
     
+    //getElementById
+    // var logo = document.getElementById('logo');   
+    // var navegacion = document.getElementById('navegacion');
     
+
+    //GetElementsByClassName
+    // var navegacion = document.getElementsByClassName('navegacion');
+    // var contenido = document.getElementsByClassName('contenido');
+
+    // console.log(contenido);
+
+
+    //getElementsByTagName
+    var enlaces = document.getElementsByTagName('a');
+    console.log(enlaces.length);
+
+    for (let i = 0; i < enlaces.length; i++) {
+      enlaces[i].setAttribute('target','_blank');
       
+    }
+
+    var enlacesSidebar = document.getElementById('sidebar').getElementsByTagName('a');
+    console.log(enlacesSidebar);
+
+    for (let i = 0; i < enlacesSidebar.length; i++) {
+      enlacesSidebar[i].setAttribute('href','http://www.google.com');
       
-    
+    }
+
+
     
   });
   
